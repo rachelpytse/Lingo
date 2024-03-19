1b38fb46bf232b08ad77fbc4d94a62504e30f888
rach <rachelpytse@gmail.com>
2024-03-19 23:19:53 +0100

    added lesson header

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,8 +1,35 @@
-const LessonPage = () => {
+import { getLesson, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+import { Quiz } from "./quiz";
+
+const LessonPage = async () => {
+    const lessonData = getLesson()
+    const userProgressData = getUserProgress()
+
+    const [
+        lesson,
+        userProgress,
+    ] = await Promise.all([
+        lessonData,
+        userProgressData
+    ])
+
+    if(!lesson || !userProgress) {
+        redirect("/learn")
+    }
+
+    const initialPercentage = lesson.challenges
+        .filter((challenge) => challenge.completed)
+        .length / lesson.challenges.length * 100
+
     return ( 
-        <div>
-            Lesson page
-        </div>
+        <Quiz
+            initialLessonId={lesson.id}
+            initialLessonChallenges={lesson.challenges}
+            initialHearts={userProgress.hearts}
+            initialPercentage={initialPercentage}
+            userSubscription={null}
+        />
      );
 }
  
